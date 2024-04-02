2cff1d3b4fbf2aed5e8898e105b32384dfc3cc48
Daniele Tricoli <eriol@mornie.org>
2024-04-03 01:43:10 +0200

    Add a modal for the menu

--- mhw2.js
+++ mhw2.js
@@ -1,11 +1,12 @@
-
 const menu = document.querySelector('#menu');
 const menuPanel = document.querySelector('#menu_panel');
+const menuModal = document.querySelector('#menu_modal');
 
 menu.addEventListener('click', onMenuClicked);
 
 function onMenuClicked(event) {
-    console.log('menu clicked');
-    event.stopPropagation();
-    menuPanel.classList.toggle('hidden');
+  event.stopPropagation();
+  menuPanel.classList.toggle('hidden');
+  menuModal.classList.toggle('hidden');
+  document.body.classList.toggle('no-scroll');
 }
