2d0456b48e9fa3bf45d18c64e1dc0098027b1c4f
Daniele Tricoli <eriol@mornie.org>
2024-04-03 03:23:34 +0200

    Change the menu bars icon into a x-mark when menu is clicked

--- mhw2.js
+++ mhw2.js
@@ -1,12 +1,27 @@
 const menu = document.querySelector('#menu');
 const menuPanel = document.querySelector('#menu_panel');
 const menuModal = document.querySelector('#menu_modal');
+const menuIcon = document.querySelector('#menu img');
+
+const MENU_OPEN = 'images/bars-solid.svg';
+const MENU_CLOSE = 'images/xmark-solid.svg';
 
 menu.addEventListener('click', onMenuClicked);
 
+// Open the side menu panel on click.
 function onMenuClicked(event) {
   event.stopPropagation();
+
   menuPanel.classList.toggle('hidden');
   menuModal.classList.toggle('hidden');
   document.body.classList.toggle('no-scroll');
+  // We use a data attribute to save the menu state in order
+  // to change the menu icon.
+  if (menuIcon.dataset.state === 'open') {
+    menuIcon.src = MENU_CLOSE;
+    menuIcon.dataset.state = 'close';
+  } else {
+    menuIcon.dataset.state = 'open';
+    menuIcon.src = MENU_OPEN;
+  }
 }
